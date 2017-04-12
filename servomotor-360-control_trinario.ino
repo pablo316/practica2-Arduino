517f4e6236653fd3665fb76a1fa968e84386008f
pablo316 <lecter.whs@gmail.com>
2017-04-12 17:11:03 -0300

    Add files via upload

---
+++ servomotor-360-control_trinario.ino
@@ -0,0 +1,64 @@
+
+#include <Servo.h>
+
+Servo myservo;  // create servo object to control a servo
+// twelve servo objects can be created on most boards
+
+int pos = 0;    // variable to store the servo position
+
+int incoming = 0;   // for incoming serial data
+
+void setup() {
+  Serial.begin(9600);     // opens serial port, sets data rate to 9600 bps
+  //myservo.attach(9);  // attaches the servo on pin 9 to the servo object //
+}
+
+void loop() {
+
+  // send data only when you receive data:
+  if (Serial.available() > 0) {
+    // read the incoming byte:
+    incoming = Serial.parseInt();
+
+    // say what you got:
+    Serial.print("dato recibido: ");
+    Serial.println(incoming);
+
+
+//ingresar 2 motor retrocede
+if(incoming==2){
+myservo.attach(9); 
+myservo.write(180);
+delay(2000);
+Serial.println("retrocedio: ");
+incoming=0;
+}
+
+//ingresar 2 motor avanza
+if(incoming==1){
+  myservo.attach(9); 
+  myservo.write(0);
+  Serial.println("avanzo: ");
+  delay(2000);
+  incoming=0;
+  }
+  
+if(incoming == 0) {
+  myservo.detach(); 
+  Serial.println("paro: ");
+  Serial.println(" ");
+  }
+
+  
+
+  }
+
+
+
+
+  
+  }
+  
+
+
+
---
+++ servomotor360char/servomotor360char.ino
@@ -0,0 +1,52 @@
+#include <Servo.h>
+
+Servo myservo;  // create servo object to control a servo
+// twelve servo objects can be created on most boards
+
+
+void setup() {
+  // put your setup code here, to run once:
+  Serial.begin(9600);
+}
+
+void loop() {
+  //myservo.attach(9); // Asocia la variable al pin donde está conectado el servo.
+  //delay(5000);
+        char dato = Serial.read();
+        if(dato == 'w'){
+            myservo.attach(9);
+          Serial.println("avanza a velocidad 120");
+          myservo.write(120); // Gira lentamente en una dirección durante 4 segundos.
+          
+        }
+
+        if(dato =='s'){
+          myservo.attach(9);
+          Serial.println("retrocede a velocidad 60");
+          myservo.write(60); // Gira lentamente en una dirección durante 4 segundos.
+          }
+        if(dato =='c'){
+           myservo.detach();
+          Serial.println("detener");
+        }
+        if(dato =='d'){
+          myservo.attach(9);
+          Serial.println("envio d");
+          myservo.write(120); // Gira lentamente en una dirección durante 4 segundos.
+          delay(3000);
+          myservo.detach();
+          Serial.println("detener");
+        }
+        if(dato =='e'){
+          myservo.attach(9);
+          Serial.println("envio e");
+          myservo.write(98); // Gira lentamente en una dirección durante 4 segundos.
+          delay(3000);
+          myservo.detach();
+          Serial.println("detener");
+        }
+  //delay(5000);
+ //myservo.detach(); // Desasocia la variable Servo de su pin
+
+  
+}
